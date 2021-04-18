0854132219d16a3a3ad5458bd711edf24660895a
Lucas Torres <jlmax.torres@gmail.com>
2021-04-18 19:14:04 -0400

    Update slack helper

--- menus/helpers.py
+++ menus/helpers.py
@@ -2,33 +2,25 @@ import logging
 
 from django.conf import settings
 
-import slack
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
-TEST_CHANNEL = settings.SLACK_TEST_CHANNEL
 
-
-def post_to_user_slack(username, message=None, attachment=None):
-    """Send a message to specific username using a slack display username or"""
-    client = slack.WebClient(token=settings.SLACK_BOT_TOKEN)
+def post_to_user_slack(username, message, attachment=None):
+    """Send a message to specific username using the slack user ID"""
+    client = WebClient(token=settings.SLACK_BOT_TOKEN)
     try:
-        # Verify slack display's username matches with existing
-        # slack member's id
-        # request = client.api_call("users.list")
-        # if request["ok"]:
-        #     for item in request["members"]:
-        #         if item["id"] == username:
-        #             username = item["id"]
         client.chat_postMessage(
             channel=username,
             text=message,
             attachments=attachment,
             as_user=True,
         )
-    except Exception as e:
-        # TODO: notify someone this exception
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["ok"] is False
+        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        print(f"SLACK SDK got an error: {e.response['error']}")
         if settings.DEBUG:
-            # Hook into the slack logger
-            # logger = logging.basicConfig()
-            logger = logging.getLogger("slack")
-            logger.setLevel(logging.DEBUG)
-            logger.error(e)
+            logger = logging.getLogger("console")
+            logger.error(e.response["error"])
